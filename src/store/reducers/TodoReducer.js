f5ca368cf1b0481f23fa08f01f5d0dab4c7afca4
Mehmet Bulam <mehmet3bulam@gmail.com>
2022-04-02 19:16:06 +0300

    * 'FILTER_FOR_USER' durumu eklendi. * 'EDIT_TODO' durumu eklendi. * 'ADD_TODO' ve 'REMOVE_TODO' yeni başlangıç değerine göre düzenlendi.

--- src/store/reducers/TodoReducer.js
+++ src/store/reducers/TodoReducer.js
@@ -1,15 +1,34 @@
 import uuid from "react-uuid/uuid";
 
-export const TodoReducer = (state, action) => {
+export const TodoReducer = (todos, action) => {
     switch (action.type) {
+        case 'FILTER_FOR_USER':
+            return {
+                data: [
+                    ...todos.data
+                ],
+                filterData: todos.data.filter(todo => todo.userId === action.loginUserId || action.admin === true)
+            }
         case 'ADD_TODO':
-            return [
-                ...state,
-                { id: uuid(), userId: action.userid, text: action.text }
-            ];
+            return {
+                data: [
+                    ...todos.data,
+                    { id: uuid(), userId: action.userid, text: action.text }
+                ],
+                filterData: []
+            };
         case 'REMOVE_TODO':
-            return state.filter(todo => todo.id !== action.id);
+            return {
+                data: [
+                    ...todos.data.filter(todo => todo.id !== action.id)
+                ],
+                filterData: []
+            };
+        case 'EDIT_TODO':
+            const todoIndex = todos.data.findIndex(todo => todo.id == action.id);
+            todos.data[todoIndex].text = action.editTodo;
+            return todos;
         default:
-            return state;
+            return todos;
     }
 };
