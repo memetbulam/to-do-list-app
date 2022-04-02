078b0bffef982cec47a61e54b7898ec979a15cbe
Mehmet Bulam <mehmet3bulam@gmail.com>
2022-04-02 19:12:08 +0300

    Başlangıç durumu değiştirildi.

--- src/store/contexts/TodoListContext.js
+++ src/store/contexts/TodoListContext.js
@@ -5,17 +5,20 @@ import uuid from "react-uuid";
 export const TodoListContext = createContext();
 
 const TodoListContextProvider = ({ children }) => {
-    const [todos, todosDispatch] = useReducer(TodoReducer, [
-        { id: uuid(), userId: 1, text: "todo string example 1 " },
-        { id: uuid(), userId: 1, text: "todo string example 2 " },
-        { id: uuid(), userId: 2, text: "todo string example 1" },
-        { id: uuid(), userId: 3, text: "todo string example 2" },
-        { id: uuid(), userId: 4, text: "todo string example 1" },
-        { id: uuid(), userId: 4, text: "todo string example 2" },
-        { id: uuid(), userId: 4, text: "todo string example 3" },
-        { id: uuid(), userId: 5, text: "todo string example 1" },
-        { id: uuid(), userId: 5, text: "todo string example 2" }
-    ]);
+    const [todos, todosDispatch] = useReducer(TodoReducer, {
+        data: [
+            { id: uuid(), userId: 1, text: "todo text 1" },
+            { id: uuid(), userId: 1, text: "todo text 2" },
+            { id: uuid(), userId: 2, text: "todo text 3" },
+            { id: uuid(), userId: 3, text: "todo text 4" },
+            { id: uuid(), userId: 4, text: "todo text 5" },
+            { id: uuid(), userId: 4, text: "todo text 6" },
+            { id: uuid(), userId: 4, text: "todo text 7" },
+            { id: uuid(), userId: 5, text: "todo text 8" },
+            { id: uuid(), userId: 5, text: "todo text 9" }
+        ],
+        filterData: []
+    });
     return (
         <TodoListContext.Provider value={{ todos, todosDispatch }}>
             {children}
