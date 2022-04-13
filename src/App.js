c5d0334047d34137e2eddba7d7adc7ee74253053
Mehmet <mehmet3bulam@gmail.com>
2022-04-13 17:28:43 +0300

    fontawesome ve title import edildi.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import Routing from "./Routing";
 import TodoListContextProvider from "./store/contexts/TodoListContext";
 import UsersContextProvider from "./store/contexts/UsersContext";
+import Title from './components/title/Title';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faEraser, faPen, faPlus } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab, faPlus, faEraser, faPen);
 
 function App() {
   return (
     <div className="App">
+      <Title />
       <UsersContextProvider>
         <TodoListContextProvider>
           <Routing />
