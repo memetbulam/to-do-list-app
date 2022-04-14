bb750575dc13741642669bc633836bdf97be4e98
Mehmet <mehmet3bulam@gmail.com>
2022-04-14 13:53:38 +0300

    Filter metodu kaldırılarak getUserFromSession yardımcı fonksiyonu eklendi.

--- src/components/logout/Index.js
+++ src/components/logout/Index.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { Col, Row } from "react-bootstrap";
 import { getSession, removeSession } from "../../utils/Session";
+import { getUserFromSession } from "../../utils/helpers/GetUserFromSession";
 import { useUsersContext } from "../../hooks/useContext";
 
-export { Link, Col, Row, getSession, removeSession, useUsersContext };
+export { Link, Col, Row, getSession, removeSession, useUsersContext, getUserFromSession };
--- src/components/logout/LogOut.jsx
+++ src/components/logout/LogOut.jsx
@@ -1,14 +1,14 @@
-import { Link, Col, Row, getSession, removeSession, useUsersContext } from './Index';
+import { Link, Col, Row, getSession, removeSession, useUsersContext, getUserFromSession } from './Index';
 
 const LogOut = () => {
-    const loginUserId = getSession();
     const usersContext = useUsersContext();
-    const userName = usersContext.users.filter(user => user.id === loginUserId);
+    const loginUserId = getSession();
+    const userName = getUserFromSession(usersContext.users, loginUserId);
 
     return (
         <Row>
             <Col xs={12} className="d-flex justify-content-end align-items-center my-2">
-                <span>Hoş geldin <b>{userName[0].username}</b></span>
+                <span>Hoş geldin <b>{userName.username}</b></span>
                 <Link to="/" className="btn btn-outline-secondary ms-2" onClick={() => { removeSession() }}>Çıkış Yap</Link>
             </Col>
         </Row>
