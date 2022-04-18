9af9edab0384852ad4258ef9acf6ea22536d32bc
Mehmet <mehmet3bulam@gmail.com>
2022-04-18 20:40:45 +0300

    Formik ve addTodoValidationSchema import edildi.

--- src/components/todolist/Index.js
+++ src/components/todolist/Index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useTodoListContext, useUsersContext } from '../../hooks/useContext';
 import { getSession } from "../../utils/Session";
@@ -7,8 +7,10 @@ import LogOut from "../logout/LogOut";
 import './TodoList.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button, Form, Container, ListGroup, InputGroup } from "react-bootstrap";
+import { Formik } from "formik";
+import { addTodoValidationSchema } from "../../utils/helpers/Validations";
 
 export {
-    useEffect, useState, Link, useTodoListContext, useUsersContext, getSession, getUserFromSession,
-    LogOut, FontAwesomeIcon, Button, Form, Container, ListGroup, InputGroup
+    useEffect, Link, useTodoListContext, useUsersContext, getSession, getUserFromSession,
+    LogOut, FontAwesomeIcon, Formik, addTodoValidationSchema, Button, Form, Container, ListGroup, InputGroup
 };
