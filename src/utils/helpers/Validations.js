fd4013603a5d70a848475ec6b4b42904e625166c
Mehmet <mehmet3bulam@gmail.com>
2022-04-18 20:39:22 +0300

    Yeni Yapılacak ekleme işlemi için şema oluşturuldu.

--- src/utils/helpers/Validations.js
+++ src/utils/helpers/Validations.js
@@ -4,3 +4,7 @@ export const loginValidationSchema = () => yup.object().shape({
     userName: yup.string().trim().required("Kullanıcı Adı Alanı Boş Bırakılamaz!"),
     password: yup.string().required("Şifre Alanı Boş Bırakılamaz!")
 });
+
+export const addTodoValidationSchema = () => yup.object().shape({
+    addTodo: yup.string().trim().required("Boş Yapılacak eklenemez!").max(120, "En fazla 120 karakter girebilirsiniz!")
+});
