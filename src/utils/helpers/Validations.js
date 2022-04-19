459f52ba1e1c897f888a307f9830a99b9f038209
Mehmet <mehmet3bulam@gmail.com>
2022-04-19 12:59:06 +0300

    editTodoValidationSchema oluşturuldu.

--- src/utils/helpers/Validations.js
+++ src/utils/helpers/Validations.js
@@ -8,3 +8,8 @@ export const loginValidationSchema = () => yup.object().shape({
 export const addTodoValidationSchema = () => yup.object().shape({
     addTodo: yup.string().trim().required("Boş Yapılacak eklenemez!").max(120, "En fazla 120 karakter girebilirsiniz!")
 });
+
+export const editTodoValidationSchema = () => yup.object().shape({
+    editTodo: yup.string().trim().required("En az 1 karakter giriniz veya 'VAZGEÇ' butonuna tıklayınız!")
+        .max(120, "En fazla 120 karakter girebilirsiniz!")
+});
